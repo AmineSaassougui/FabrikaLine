affc86aa9fcbd021dd1469c5715ca2706641eab8
Med Amine Saassougui <93150838+AmineSaassougui@users.noreply.github.com>
2024-02-25 17:31:59 +0100

    item mangment

--- FabrikaLine_Backend/src/main/java/com/example/fabrikaline_backend/Repositories/IOrderStatusRepository.java
+++ FabrikaLine_Backend/src/main/java/com/example/fabrikaline_backend/Repositories/IOrderStatusRepository.java
@@ -2,5 +2,8 @@ package com.example.fabrikaline_backend.Repositories;
 import com.example.fabrikaline_backend.Entities.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IOrderStatusRepository extends JpaRepository<OrderStatus,Long> {
+    List<OrderStatus> findByDescriptionContaining(String searchCriteria);
 }
