4810c6927e335501ddba99e03bcf9e91ffaf78cb
Med Amine Saassougui <93150838+AmineSaassougui@users.noreply.github.com>
2024-02-25 18:43:05 +0100

    userType advancedSearch

--- FabrikaLine_Backend/src/main/java/com/example/fabrikaline_backend/Repositories/IUserTypeRepository.java
+++ FabrikaLine_Backend/src/main/java/com/example/fabrikaline_backend/Repositories/IUserTypeRepository.java
@@ -2,5 +2,8 @@ package com.example.fabrikaline_backend.Repositories;
 import com.example.fabrikaline_backend.Entities.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IUserTypeRepository extends JpaRepository<UserType,Long> {
+    List<UserType> findByDescriptionContaining(String searchCriteria);
 }
