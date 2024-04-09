dcd067a5578539eacbf440489ddbfeb2e3d023d4
Rahmouni-Seif-BI <seif2000rahmouni@gmail.com>
2024-04-09 23:43:36 +0100

    order + orderLine services

---
+++ frontend/FabrikaApp/src/app/views/home/order-line/order-line-list.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { GridComponent } from '@syncfusion/ej2-angular-grids';
+import { OrderLineRestControllerService } from './../../../../../libs/openapi/src/api/orderLineRestController.service';
+
+@Component({
+  selector: 'app-order-line-list',
+  templateUrl: './order-line-list.component.html',
+  styleUrls: ['./order-line-list.component.css']
+})
+export class ItemCategoryListComponent implements OnInit {
+
+
+
+  constructor(private route: Router, private _service: OrderLineRestControllerService) { }
+  ngOnInit(): void {
+    this.load();
+  }
+
+  @ViewChild('grid') public grid!: GridComponent;
+
+  load() {
+    this._service.getAll4().subscribe((data: any[]) => {
+      this.grid.dataSource = data;
+    });
+  }
+
+
+  delete() {
+    if (this.grid.getSelectedRecords().length != 1) {
+      alert('Sélectionner une ligne!');
+    }
+    else {
+      let selectedrecord: any = this.grid.getSelectedRecords()[0]; // get the selected records.
+      this._service.deleteOrderLine(selectedrecord['id']).subscribe((data: any) => {
+        this.load()
+      });
+    }
+  }
+
+
+  public onDoubleClick(event: any): void {
+    this.edit(event.rowData);
+  }
+
+  modify() {
+    if (this.grid.getSelectedRecords().length != 1) {
+      alert('Sélectionner une ligne!');
+    }
+    else {
+      let selectedrecord: any = this.grid.getSelectedRecords()[0]; 
+      this.edit(selectedrecord);
+    }
+  }
+
+  public edit(selectedrecord: any) {
+
+    const navigationExtras: NavigationExtras = {
+      state: {
+        id: selectedrecord.id,
+      }
+    };
+    var routerPath = `/Adm/ItemCategoryForm`;
+    this.route.navigateByUrl(routerPath, navigationExtras);
+  }
+}
---
+++ frontend/FabrikaApp/src/app/views/home/order/order-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderControllerService } from 'libs/openapi/src';
+
+@Component({
+  selector: 'app-order-form',
+  templateUrl: './order-form.component.html',
+  styleUrls: ['./order-form.component.css']
+})
+export class OrderFormComponent implements OnInit {
+
+  object: any = {}; // Initialize object with empty object
+
+  constructor(private route: Router, private _service: OrderControllerService) { }
+  id: any;
+
+  ngOnInit() {
+    this.id = history.state.id;
+    if (this.id != null) {
+      this.object.id = this.id;
+      this.load();
+    }
+  }
+
+  save(object: any) {
+  
+    this._service.save5(object).subscribe((res: any) => {
+      if (res != null) {
+        this.route.navigate(['/Adm/OrderList'])
+      } else {
+        alert("Something went wrong!")
+      }
+    });
+  }
+
+  load(){
+    this._service.load6(this.id).subscribe((res: any) => {   
+      this.object = res;
+    });
+  }
+
+  cancel() {
+    this.route.navigate(['/Adm/OrderList'])
+  }
+}
